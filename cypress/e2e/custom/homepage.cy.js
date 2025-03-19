d089c1a95d9e6f204c82c0f39a57135aeb6dbd43
Nicholas Ramsay <nicholasramsay@hotmail.com>
2025-03-19 02:34:57 -0700

    added some additional tests.

--- cypress/e2e/custom/homepage.cy.js
+++ cypress/e2e/custom/homepage.cy.js
@@ -35,8 +35,39 @@ describe('Restful Booker Front end', () => {
         cy.get('@room').should('have.attr', 'src', '/images/room2.jpg');
     })
 
-    it('Danger alert when missing info', () => {
+    it('Danger alert when missing booking info', () => {
+        cy.get('img[alt="Preview image of room 101"]').scrollIntoView();
         cy.get('.btn.btn-outline-primary.float-right.openBooking').first().click();
+
+        cy.get('input[name="firstname"]').type('Test');
+        cy.get('input[name="lastname"]').type('User');
+        cy.get('input[name="email"]').type('testuser123@email.com');
+        cy.get('input[name="phone"]').type('123456789000');
+
+        cy.get('.btn.btn-outline-primary.float-right.book-room').click();
+        cy.get('.alert-danger').should('be.visible');
+    })
+
+    it('Danger alert appears when missing contact info', () => {
+        cy.get('.contact').scrollIntoView();
+        
+        cy.get('#submitContact').click();
+        
+        cy.get('.alert-danger').should('be.visible');
+    })
+
+    it('Contact sucessfully submits message', () => {
+        cy.get('.contact').scrollIntoView();
+        cy.get('input[id="name"]').type('Test');
+        cy.get('input[id="email"]').type('testuser@email.com');
+        cy.get('input[id="phone"]').type('123456789000');
+        cy.get('input[id="subject"]').type('This is a test');
+        cy.get('textarea[id="description"]')
+          .type('This is a test message for testing out the contact fields at the bottom');
+
+        cy.get('#submitContact').click();
+                          
+        cy.get(':nth-child(2) > div > h2').contains("Thanks for getting in touch");
     })
 
 });
